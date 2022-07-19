7febcb24a68178a51aa957d54fb74850fa879930
Beatrix <beatrix@sourcegraph.com>
2022-07-19 15:18:33 -0700

    tier2 update

--- docker-compose/gcp-start.sh
+++ docker-compose/gcp-start.sh
@@ -2,49 +2,51 @@
 
 set -euxo pipefail
 
-EBS_VOLUME_DEVICE_NAME='/dev/sdb'
+PERSISTENT_DISK_DEVICE_NAME='/dev/sdb'
 DOCKER_DATA_ROOT='/mnt/docker-data'
 
 DOCKER_COMPOSE_VERSION='1.29.2'
-DEPLOY_SOURCEGRAPH_DOCKER_CHECKOUT='/home/ec2-user/deploy-sourcegraph-docker'
+DEPLOY_SOURCEGRAPH_DOCKER_CHECKOUT='/root/deploy-sourcegraph-docker'
 
 # 🚨 Update these variables with the correct values from your fork!
 DEPLOY_SOURCEGRAPH_DOCKER_FORK_CLONE_URL='https://github.com/sourcegraph/delivery-tiger-docker-compose-test.git'
 DEPLOY_SOURCEGRAPH_DOCKER_FORK_REVISION='tier2-gcp'
 
 # Install git
-yum update -y
-yum install git -y
+sudo apt-get update -y
+sudo apt-get install -y git
 
 # Clone Docker Compose definition
 git clone "${DEPLOY_SOURCEGRAPH_DOCKER_FORK_CLONE_URL}" "${DEPLOY_SOURCEGRAPH_DOCKER_CHECKOUT}"
 cd "${DEPLOY_SOURCEGRAPH_DOCKER_CHECKOUT}"
 git checkout "${DEPLOY_SOURCEGRAPH_DOCKER_FORK_REVISION}"
 
-# Format (if necessary) and mount EBS volume
-device_fs=$(lsblk "${EBS_VOLUME_DEVICE_NAME}" --noheadings --output fsType)
+# Format (if necessary) and mount GCP persistent disk
+device_fs=$(sudo lsblk "${PERSISTENT_DISK_DEVICE_NAME}" --noheadings --output fsType)
 if [ "${device_fs}" == "" ]; then ## only format the volume if it isn't already formatted
-  mkfs -t xfs "${EBS_VOLUME_DEVICE_NAME}"
+  sudo mkfs.ext4 -m 0 -E lazy_itable_init=0,lazy_journal_init=0,discard "${PERSISTENT_DISK_DEVICE_NAME}"
 fi
-mkdir -p "${DOCKER_DATA_ROOT}"
-mount "${EBS_VOLUME_DEVICE_NAME}" "${DOCKER_DATA_ROOT}"
+sudo mkdir -p "${DOCKER_DATA_ROOT}"
+sudo mount -o discard,defaults "${PERSISTENT_DISK_DEVICE_NAME}" "${DOCKER_DATA_ROOT}"
 
-# Mount EBS volume on reboots
-EBS_UUID=$(blkid -s UUID -o value "${EBS_VOLUME_DEVICE_NAME}")
-echo "UUID=${EBS_UUID}  ${DOCKER_DATA_ROOT}  xfs  defaults,nofail  0  2" >>'/etc/fstab'
+# Mount GCP disk on reboots
+DISK_UUID=$(sudo blkid -s UUID -o value "${PERSISTENT_DISK_DEVICE_NAME}")
+sudo echo "UUID=${DISK_UUID}  ${DOCKER_DATA_ROOT}  ext4  discard,defaults,nofail  0  2" >>'/etc/fstab'
 umount "${DOCKER_DATA_ROOT}"
 mount -a
 
-# Install, configure, and enable Docker
-yum update -y
-amazon-linux-extras install docker
-systemctl enable --now docker
-sed -i -e 's/1024/262144/g' /etc/sysconfig/docker
-sed -i -e 's/4096/262144/g' /etc/sysconfig/docker
-usermod -a -G docker ec2-user
+# Install Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-get update -y
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update -y
+apt-cache policy docker-ce
+apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # Install jq for scripting
-yum install -y jq
+sudo apt-get update -y
+sudo apt-get install -y jq
 
 # Edit Docker storage directory to mounted volume
 DOCKER_DAEMON_CONFIG_FILE='/etc/docker/daemon.json'
@@ -58,11 +60,11 @@ fi
 ## update Docker's 'data-root' to point to our mounted disk
 tmp_config=$(mktemp)
 trap "rm -f ${tmp_config}" EXIT
-cat "${DOCKER_DAEMON_CONFIG_FILE}" | jq --arg DATA_ROOT "${DOCKER_DATA_ROOT}" '.["data-root"]=$DATA_ROOT' >"${tmp_config}"
-cat "${tmp_config}" >"${DOCKER_DAEMON_CONFIG_FILE}"
+sudo cat "${DOCKER_DAEMON_CONFIG_FILE}" | sudo jq --arg DATA_ROOT "${DOCKER_DATA_ROOT}" '.["data-root"]=$DATA_ROOT' >"${tmp_config}"
+sudo cat "${tmp_config}" >"${DOCKER_DAEMON_CONFIG_FILE}"
 
 ## finally, restart Docker daemon to pick up our changes
-systemctl restart --now docker
+sudo systemctl restart --now docker
 
 # Install Docker Compose
 curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
