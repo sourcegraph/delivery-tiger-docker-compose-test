33447bc887bb63fc57d5cdc362953c5e6f79bf79
Beatrix <beatrix@sourcegraph.com>
2022-07-20 13:45:10 -0700

    add tier 3 gcp

--- docker-compose/gcp-start.sh
+++ docker-compose/gcp-start.sh
@@ -10,7 +10,7 @@ DEPLOY_SOURCEGRAPH_DOCKER_CHECKOUT='/root/deploy-sourcegraph-docker'
 
 # 🚨 Update these variables with the correct values from your fork!
 DEPLOY_SOURCEGRAPH_DOCKER_FORK_CLONE_URL='https://github.com/sourcegraph/delivery-tiger-docker-compose-test.git'
-DEPLOY_SOURCEGRAPH_DOCKER_FORK_REVISION='tier2-gcp'
+DEPLOY_SOURCEGRAPH_DOCKER_FORK_REVISION='tier3-gcp'
 
 # Install git
 sudo apt-get update -y
