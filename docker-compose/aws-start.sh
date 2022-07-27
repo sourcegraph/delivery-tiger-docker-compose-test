0e7df114586e42a83ea6e44ee5030547cf3bfa8d
Beatrix <beatrix@sourcegraph.com>
2022-07-27 12:00:08 -0700

    Add more resources

--- docker-compose/aws-start.sh
+++ docker-compose/aws-start.sh
@@ -23,15 +23,16 @@ git checkout "${DEPLOY_SOURCEGRAPH_DOCKER_FORK_REVISION}"
 
 # Format (if necessary) and mount EBS volume
 device_fs=$(lsblk "${EBS_VOLUME_DEVICE_NAME}" --noheadings --output fsType)
-if [ "${device_fs}" == "" ]; then ## only format the volume if it isn't already formatted
-    mkfs -t xfs "${EBS_VOLUME_DEVICE_NAME}"
+if [ "${device_fs}" == "" ] ## only format the volume if it isn't already formatted
+then
+  mkfs -t xfs "${EBS_VOLUME_DEVICE_NAME}"
 fi
 mkdir -p "${DOCKER_DATA_ROOT}"
 mount "${EBS_VOLUME_DEVICE_NAME}" "${DOCKER_DATA_ROOT}"
 
 # Mount EBS volume on reboots
 EBS_UUID=$(blkid -s UUID -o value "${EBS_VOLUME_DEVICE_NAME}")
-echo "UUID=${EBS_UUID}  ${DOCKER_DATA_ROOT}  xfs  defaults,nofail  0  2" >>'/etc/fstab'
+echo "UUID=${EBS_UUID}  ${DOCKER_DATA_ROOT}  xfs  defaults,nofail  0  2" >> '/etc/fstab'
 umount "${DOCKER_DATA_ROOT}"
 mount -a
 
@@ -50,16 +51,17 @@ yum install -y jq
 DOCKER_DAEMON_CONFIG_FILE='/etc/docker/daemon.json'
 
 ## initialize the config file with empty json if it doesn't exist
-if [ ! -f "${DOCKER_DAEMON_CONFIG_FILE}" ]; then
-    mkdir -p $(dirname "${DOCKER_DAEMON_CONFIG_FILE}")
-    echo '{}' >"${DOCKER_DAEMON_CONFIG_FILE}"
+if [ ! -f "${DOCKER_DAEMON_CONFIG_FILE}" ]
+then
+  mkdir -p $(dirname "${DOCKER_DAEMON_CONFIG_FILE}")
+  echo '{}' > "${DOCKER_DAEMON_CONFIG_FILE}"
 fi
 
 ## update Docker's 'data-root' to point to our mounted disk
 tmp_config=$(mktemp)
 trap "rm -f ${tmp_config}" EXIT
-cat "${DOCKER_DAEMON_CONFIG_FILE}" | jq --arg DATA_ROOT "${DOCKER_DATA_ROOT}" '.["data-root"]=$DATA_ROOT' >"${tmp_config}"
-cat "${tmp_config}" >"${DOCKER_DAEMON_CONFIG_FILE}"
+cat "${DOCKER_DAEMON_CONFIG_FILE}" | jq --arg DATA_ROOT "${DOCKER_DATA_ROOT}" '.["data-root"]=$DATA_ROOT' > "${tmp_config}"
+cat "${tmp_config}" > "${DOCKER_DAEMON_CONFIG_FILE}"
 
 ## finally, restart Docker daemon to pick up our changes
 systemctl restart --now docker
