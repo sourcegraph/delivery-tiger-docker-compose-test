438e067bde3ecca1222f9ad29965a5eef1c0c4f7
James Cotter <james.cotter@sourcegraph.com>
2022-09-01 12:16:11 +0100

    initial gcp setup

---
+++ docker-compose/gcp-start.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+PERSISTENT_DISK_DEVICE_NAME='/dev/sdb'
+DOCKER_DATA_ROOT='/mnt/docker-data'
+
+DOCKER_COMPOSE_VERSION='1.29.2'
+DEPLOY_SOURCEGRAPH_DOCKER_CHECKOUT='/root/deploy-sourcegraph-docker'
+
+# 🚨 Update these variables with the correct values from your fork!
+DEPLOY_SOURCEGRAPH_DOCKER_FORK_CLONE_URL='https://github.com/sourcegraph/delivery-tiger-docker-compose-test.git'
+DEPLOY_SOURCEGRAPH_DOCKER_FORK_REVISION='size-m'
+
+# Install git
+sudo apt-get update -y
+sudo apt-get install -y git
+
+# Clone Docker Compose definition
+git clone "${DEPLOY_SOURCEGRAPH_DOCKER_FORK_CLONE_URL}" "${DEPLOY_SOURCEGRAPH_DOCKER_CHECKOUT}"
+cd "${DEPLOY_SOURCEGRAPH_DOCKER_CHECKOUT}"
+git checkout "${DEPLOY_SOURCEGRAPH_DOCKER_FORK_REVISION}"
+
+# Format (if necessary) and mount GCP persistent disk
+device_fs=$(sudo lsblk "${PERSISTENT_DISK_DEVICE_NAME}" --noheadings --output fsType)
+if [ "${device_fs}" == "" ] ## only format the volume if it isn't already formatted
+then
+    sudo mkfs.ext4 -m 0 -E lazy_itable_init=0,lazy_journal_init=0,discard "${PERSISTENT_DISK_DEVICE_NAME}"
+fi
+sudo mkdir -p "${DOCKER_DATA_ROOT}"
+sudo mount -o discard,defaults "${PERSISTENT_DISK_DEVICE_NAME}" "${DOCKER_DATA_ROOT}"
+
+# Mount GCP disk on reboots
+DISK_UUID=$(sudo blkid -s UUID -o value "${PERSISTENT_DISK_DEVICE_NAME}")
+sudo echo "UUID=${DISK_UUID}  ${DOCKER_DATA_ROOT}  ext4  discard,defaults,nofail  0  2" >> '/etc/fstab'
+umount "${DOCKER_DATA_ROOT}"
+mount -a
+
+# Install Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-get update -y
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update -y
+apt-cache policy docker-ce
+apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# Install jq for scripting
+sudo apt-get update -y
+sudo apt-get install -y jq
+
+# Edit Docker storage directory to mounted volume
+DOCKER_DAEMON_CONFIG_FILE='/etc/docker/daemon.json'
+
+## initialize the config file with empty json if it doesn't exist
+if [ ! -f "${DOCKER_DAEMON_CONFIG_FILE}" ]
+then
+    mkdir -p $(dirname "${DOCKER_DAEMON_CONFIG_FILE}")
+    echo '{}' > "${DOCKER_DAEMON_CONFIG_FILE}"
+fi
+
+## update Docker's 'data-root' to point to our mounted disk
+tmp_config=$(mktemp)
+trap "rm -f ${tmp_config}" EXIT
+sudo cat "${DOCKER_DAEMON_CONFIG_FILE}" | sudo jq --arg DATA_ROOT "${DOCKER_DATA_ROOT}" '.["data-root"]=$DATA_ROOT' > "${tmp_config}"
+sudo cat "${tmp_config}" > "${DOCKER_DAEMON_CONFIG_FILE}"
+
+## finally, restart Docker daemon to pick up our changes
+sudo systemctl restart --now docker
+
+# Install Docker Compose
+curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+curl -L "https://raw.githubusercontent.com/docker/compose/${DOCKER_COMPOSE_VERSION}/contrib/completion/bash/docker-compose" -o /etc/bash_completion.d/docker-compose
+
+# Run Sourcegraph. Restart the containers upon reboot.
+cd "${DEPLOY_SOURCEGRAPH_DOCKER_CHECKOUT}"/docker-compose
+docker-compose up -d
